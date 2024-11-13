4ad8084ca52146e411ebd15fd0bd1e21e66767ba
Do_Not_Dusturb <152636544+Do-Not-Dusturb@users.noreply.github.com>
2024-11-13 16:25:04 -0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,19 @@
+git push main.py
+git push full
+git puh main.origin
+// command center fire
+  drone fire
+  echo "# Python-crane" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/Do-Not-Dusturb/Python-crane.git
+git push -u origin main
+git add
+git push remote 
+drone fire
+drone stop
+'[Otu26']+ 
+if & var= 90
+set stop to stop
